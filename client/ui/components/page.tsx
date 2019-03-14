02b2ae30febdc1d47937b3a4b1e3d663638d0b32
neet <n33t5hin@gmail.com>
2019-03-15 04:54:17 +0900

    Fix sidebar scrollable

--- client/ui/components/page.tsx
+++ client/ui/components/page.tsx
@@ -1,9 +1,10 @@
 import { styled } from 'client/ui/styles';
+import { bannerHeight } from 'client/ui/styles/constants';
 
 export const Page = styled.main`
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr;
+  grid-template-rows: calc(100vh - ${bannerHeight}px);
   flex: 1 1;
 
   @media screen and (min-width: 700px) {
