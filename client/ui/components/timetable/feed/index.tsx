b9504d3e588170f173c4d791402d260c928d6a8b
neet <n33t5hin@gmail.com>
2019-03-16 18:22:16 +0900

    Improve marker positioning algorithm

--- client/ui/components/timetable/feed/index.tsx
+++ client/ui/components/timetable/feed/index.tsx
@@ -1,19 +1,15 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { styled } from 'client/ui/styles';
-import { Marker } from './marker';
+import { Marker, MarkerProps } from './marker';
 import { Event } from 'shared/entities/event';
 import { Dayjs } from 'dayjs';
-import { isOverlapping } from 'client/ui/helpers/is-overlapping';
 import { sortEvents } from 'client/ui/helpers/sort-events';
+import { isOverlapping } from 'client/ui/helpers/is-overlapping';
+import { Omit } from 'type-zoo';
 
 export interface FeedProps {
   events: Event[];
   startDate: Dayjs;
-}
-
-export interface Row {
-  id: number;
-  row: number;
 }
 
 const List = styled.ul`
@@ -32,43 +28,70 @@ const ListItem = styled.li`
 export const Feed = (props: FeedProps) => {
   const { events, startDate } = props;
 
-  const rows = useMemo(
+  /**
+   * Calculate the position of markers as a 2-dimentional array.
+   * each index of an array represents the Y position (row) of the marker
+   * @param events Sorted events to get the position
+   * @param result A 2-dimentional array of events per each row
+   */
+  const getMarkerPositions = useCallback(
+    (events: Event[], result: Event[][] = []): Event[][] => {
+      if (!events.length) {
+        return result;
+      }
+
+      // Init current row
+      result.push([]);
+      const current = result[result.length - 1];
+
+      const rest = events.reduce<Event[]>((rest, event) => {
+        if (!current.length) {
+          current.push(event);
+          return rest;
+        }
+
+        const prev = current[current.length - 1];
+
+        if (!isOverlapping(event, prev)) {
+          current.push(event);
+          return rest;
+        }
+
+        rest.push(event);
+        return rest;
+      }, []);
+
+      return getMarkerPositions(rest, result);
+    },
+    [],
+  );
+
+  // Map rows and events
+  const markers = useMemo(
     () =>
-      events.reduce<Row[]>((positions, event) => {
-        const overlappingEvents = events
-          .filter(
-            event2 => event.id !== event2.id && isOverlapping(event, event2),
-          )
-          .sort(sortEvents);
-
-        const oeRows = overlappingEvents
-          .map(event2 => positions.find(({ id }) => id === event2.id))
-          .filter((row): row is Row => !!row);
-
-        const deepestRow =
-          oeRows.reduce((result, event2) => Math.max(result, event2.row), 0) +
-          1;
-        const previousRow = oeRows.length
-          ? oeRows[oeRows.length - 1].row + 1
-          : 0;
-        const somethingNice = previousRow > oeRows.length ? 0 : previousRow;
-        const nextRow = deepestRow > oeRows.length ? somethingNice : deepestRow;
-
-        positions.push({ id: event.id, row: nextRow });
-        return positions;
-      }, []),
+      getMarkerPositions(events.sort(sortEvents))
+        .map((row, i) =>
+          row.map(
+            (event): Required<Omit<MarkerProps, 'startDate'>> => ({
+              event,
+              row: i,
+            }),
+          ),
+        )
+        .flat()
+        .sort((a, b) => sortEvents(a.event, b.event)),
     [events],
   );
 
   return (
     <List role="feed">
-      {events.map((event, i) => (
+      {markers.map(({ event, row }, i) => (
         <ListItem
           key={`${event.id}-${i}`}
           aria-setsize={events.length}
           aria-posinset={i + 1}
         >
-          <Marker event={event} row={rows[i].row} startDate={startDate} />
+          <Marker event={event} row={row} startDate={startDate} />
         </ListItem>
       ))}
     </List>
