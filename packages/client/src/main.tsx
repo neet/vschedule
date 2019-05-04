05db65282fec16dc9f372a296eb24574ea0bb3d6
Neetshin <n33t5hin@gmail.com>
2019-05-05 01:39:20 +0900

    Add introspection result

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,14 +2,19 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider } from 'react-apollo-hooks';
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher,
+} from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import { Root } from './views/root';
 import { BrowserRouter } from 'react-router-dom';
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import typeDefs from '@ril/schema/schema.gql';
+import introspectionResult from 'src/generated/introspection-result';
 
-async () => {
+(async () => {
   if (process.env.NODE_ENV === 'production') {
     OfflinePluginRuntime.install();
   }
@@ -20,16 +25,22 @@ async () => {
     return;
   }
 
-  const cache = new InMemoryCache();
-  const link = new HttpLink({ uri: 'https://localhost:3000/graphql' });
+  const fragmentMatcher = new IntrospectionFragmentMatcher({
+    introspectionQueryResultData: introspectionResult,
+  });
+
+  const cache = new InMemoryCache({ fragmentMatcher });
+  const link = new HttpLink({ uri: '/graphql' });
   const client = new ApolloClient({ cache, link, typeDefs });
 
   ReactDOM.render(
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Root />
-      </BrowserRouter>
+      <ApolloHooksProvider client={client}>
+        <BrowserRouter>
+          <Root />
+        </BrowserRouter>
+      </ApolloHooksProvider>
     </ApolloProvider>,
     mountNode,
   );
-};
+})();
