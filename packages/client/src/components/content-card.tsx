a871bccc81411cd8d409e1cc4d9e558d5348e355
Neetshin <n33t5hin@gmail.com>
2019-07-28 02:06:07 +0900

    Fix lint error

--- packages/client/src/components/content-card.tsx
+++ packages/client/src/components/content-card.tsx
@@ -89,9 +89,7 @@ export const ContentCard = (props: ContentCardProps) => {
 
   const videoId = querystring.parse(content.url.split('?')[1]).v as string;
   const thumbnailImageUrl = getThumbnailImageUrl(videoId);
-  const textForScreenReader = `${content.name} ${content.source.name} ${
-    content.description
-  }`;
+  const textForScreenReader = `${content.name} ${content.source.name} ${content.description}`;
 
   return (
     <Wrapper
