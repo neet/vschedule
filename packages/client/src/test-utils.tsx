de467d7bc4a50d2bafadbb185c8d12550dacb5ce
Neetshin <n33t5hin@gmail.com>
2019-08-25 14:44:41 +0900

    Remove object

--- packages/client/src/test-utils.tsx
+++ packages/client/src/test-utils.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'src/styles';
 import { theme as defaultTheme } from 'src/styles/theme';
 
-export const withTheme = <P extends object>(
+export const withTheme = <P extends {}>(
   Component: React.ComponentType<P>,
   theme = defaultTheme,
 ): React.SFC<P> => props => {
