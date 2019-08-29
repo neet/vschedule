071ca3d73174fd55ba705b02b3bbc4950696e59c
Neetshin <n33t5hin@gmail.com>
2019-08-29 16:25:44 +0900

    Init groups

---
+++ packages/server/src/utils/groups.ts
@@ -0,0 +1,10 @@
+const TsukinoMito = '32';
+const HiguchiKaede = '38';
+const ShizukaRin = '11';
+
+export const groups = [
+  {
+    name: 'JK組',
+    ids: [TsukinoMito, HiguchiKaede, ShizukaRin],
+  },
+];
