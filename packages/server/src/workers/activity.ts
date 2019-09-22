3a77708b09a94b3441a38272844804b19702be45
Neetshin <n33t5hin@gmail.com>
2019-09-23 00:34:11 +0900

    Make ActivityCron not fetch when existing

--- packages/server/src/workers/activity.ts
+++ packages/server/src/workers/activity.ts
@@ -3,6 +3,7 @@ import { CronJob } from 'cron';
 import { RESOURCE_URL } from 'src/config';
 import { Connection } from 'typeorm';
 import { ActivityRepository } from 'src/repository/activity';
+import { Activity } from 'src/entity/activity';
 
 export class ActivityCron {
   private readonly db: Connection;
@@ -25,6 +26,15 @@ export class ActivityCron {
       .then(response => response.data.events);
 
     for (const event of events) {
+      const activity = await this.db.manager.findOne(
+        Activity,
+        event.id.toString(),
+      );
+
+      if (activity) {
+        continue;
+      }
+
       // TODO: Optimize this
       const liverRelationships = (await Promise.all(
         event.livers.map(async liver => {
