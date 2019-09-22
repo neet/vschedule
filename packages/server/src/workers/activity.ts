5108efc39dbaa6556859180ba8e30c9c2b57f7ad
Neetshin <n33t5hin@gmail.com>
2019-09-23 00:14:56 +0900

    Not fetch performer when alredy stored

--- packages/server/src/workers/activity.ts
+++ packages/server/src/workers/activity.ts
@@ -3,6 +3,7 @@ import { CronJob } from 'cron';
 import { RESOURCE_URL } from 'src/config';
 import { Connection } from 'typeorm';
 import { ActivityRepository } from 'src/repository/activity';
+import { Performer } from 'src/entity/performer';
 
 export class ActivityCron {
   private readonly db: Connection;
@@ -25,12 +26,22 @@ export class ActivityCron {
       .then(response => response.data.events);
 
     for (const event of events) {
-      // TODO: Optimize this
-      const liverRelationships = (await Promise.all(
-        event.livers.map(async liver => {
-          return await this.gateway.fetchLiver(liver.id);
-        }),
-      )).map(response => response.data);
+      const liverRelationships: LiverRelationships[] = [];
+
+      for (const liver of event.livers) {
+        // prettier-ignore
+        const performer = this.db.manager.findOne(Performer, liver.id.toString());
+
+        if (performer) {
+          continue;
+        }
+
+        liverRelationships.push(
+          await this.gateway
+            .fetchLiver(liver.id)
+            .then(response => response.data),
+        );
+      }
 
       this.createActivity(event, liverRelationships);
     }
