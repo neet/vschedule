6acff53730f8751dc273295ec81b802c82921798
Neetshin <n33t5hin@gmail.com>
2019-10-20 16:55:31 +0900

    Fix story

--- packages/client/src/components/button/index.stories.tsx
+++ packages/client/src/components/button/index.stories.tsx
@@ -7,7 +7,20 @@ import { Button } from '.';
 
 const stories = storiesOf('Button', module);
 
-stories.add('Primary', () => {
-  const textContent = text('Text', 'Hello');
-  return <Button onClick={action('onClick')}>{textContent}</Button>;
-});
+stories
+  .add('Primary', () => {
+    const textContent = text('Text', 'Hello');
+    return (
+      <Button appearance="primary" onClick={action('onClick')}>
+        {textContent}
+      </Button>
+    );
+  })
+  .add('Skeleton', () => {
+    const textContent = text('Text', 'Hello');
+    return (
+      <Button appearance="skeleton" onClick={action('onClick')}>
+        {textContent}
+      </Button>
+    );
+  });
