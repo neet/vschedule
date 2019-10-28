4f3d5ca82295f1771c38bd3d5308352c999a90a9
Neetshin <n33t5hin@gmail.com>
2019-10-28 14:05:21 +0900

    Introduce io-ts to gateway

--- packages/gateway/src/entities.ts
+++ packages/gateway/src/entities.ts
@@ -1,70 +1,78 @@
-export type Optional<T> = T | null | undefined;
+import * as t from 'io-ts';
 
-export interface Response<Data> {
-  status: 'ok' | 'ng';
-  data: Data;
-}
+export const Response = <T extends t.Mixed>(data: T) =>
+  t.type({
+    status: t.union([t.literal('ok'), t.literal('ng')]),
+    data: data,
+  });
 
-export interface Liver {
-  id: number;
-  name: string;
-  avatar: string;
-  color: string;
-  fullbody: Optional<string>;
-  english_name: Optional<string>;
-  furigana: Optional<string>;
-  description: Optional<string>;
-  public: Optional<number>;
-  position: Optional<number>;
-}
+export const optional = (type: t.Mixed) => t.union([type, t.undefined, t.null]);
 
-export interface LiverTwitterAccount {
-  id: number;
-  liver_id: number;
-  screen_name: string;
-}
+export const Liver = t.type({
+  id: t.number,
+  name: t.string,
+  avatar: t.string,
+  color: t.string,
+  fullbody: optional(t.string),
+  english_name: optional(t.string),
+  furigana: optional(t.string),
+  description: optional(t.string),
+  public: optional(t.number),
+  position: optional(t.number),
+});
 
-export interface LiverYoutubeChannel {
-  id: number;
-  liver_id: number;
-  channel: string;
-  channel_name: string;
-  creation_order: number;
-}
+export const LiverTwitterAccount = t.type({
+  id: t.number,
+  liver_id: t.number,
+  screen_name: t.string,
+});
 
-export interface LiverRelationship {
-  liver: Liver;
-  liver_twitter_account: LiverTwitterAccount;
-  liver_youtube_channel: LiverYoutubeChannel | LiverYoutubeChannel[];
-  liver_platform: Optional<[]>;
-  user_liver: Optional<null>;
-}
+export const LiverYoutubeChannel = t.type({
+  id: t.number,
+  liver_id: t.number,
+  channel: t.string,
+  channel_name: t.string,
+  creation_order: t.string,
+});
 
-export interface Genre {
-  id: number;
-  name: string;
-}
+export const LiverRelationship = t.type({
+  liver: Liver,
+  liver_twitter_account: LiverTwitterAccount,
+  liver_youtube_channel: t.union([
+    LiverYoutubeChannel,
+    t.array(LiverYoutubeChannel),
+  ]),
+  liver_platform: optional(t.array(t.undefined)),
+  user_liver: optional(t.null),
+});
 
-export interface Event {
-  id: number;
-  name: string;
-  description: string;
-  public: number;
-  url: string;
-  thumbnail: string;
-  start_date: string;
-  end_date: string;
-  recommend: boolean;
-  genre: Optional<Genre>;
-  livers: Liver[];
-}
+export const Genre = t.type({
+  id: t.number,
+  name: t.string,
+});
+
+export const Event = t.type({
+  id: t.number,
+  name: t.string,
+  description: t.string,
+  public: t.number,
+  url: t.string,
+  thumbnail: t.string,
+  start_date: t.string,
+  end_date: t.string,
+  recommend: t.boolean,
+  genre: optional(Genre),
+  livers: t.array(Liver),
+});
 
 // DEAR ICHIKARA.
 // Stop using envelope pattern and and use HTTP status codes instead.
-export type EventsResponse = Response<{ events: Event[] }>;
-export type EventResponse = Response<{ event: Event }>;
-export type LiversResponse = Response<{
-  liver_relationships: LiverRelationship[];
-}>;
-export type LiverResponse = Response<LiverRelationship>;
-export type GenresResponse = Response<{ genres: Genre[] }>;
+export const EventsResponse = Response(t.type({ events: t.array(Event) }));
+export const EventResponse = Response(t.type({ event: Event }));
+export const LiversResponse = Response(
+  t.type({
+    liver_relationships: t.array(LiverRelationship),
+  }),
+);
+export const LiverResponse = Response(LiverRelationship);
+export const GenresResponse = Response(t.type({ genres: t.array(Genre) }));
--- packages/gateway/src/gateway.ts
+++ packages/gateway/src/gateway.ts
@@ -1,4 +1,5 @@
 import * as gaxios from 'gaxios';
+import { isLeft, toError } from 'fp-ts/lib/Either';
 import {
   EventsResponse,
   EventResponse,
@@ -28,15 +29,38 @@ export class Gateway {
     return res.data;
   };
 
-  fetchEvents = () => this.get<EventsResponse>('/v1.2/events.json');
+  fetchEvents = async () => {
+    const data = await this.get('/v1.2/events.json');
+    const response = EventsResponse.decode(data);
+    if (isLeft(response)) throw toError(response.left);
+    return response.right;
+  };
 
-  fetchEvent = (id: number) =>
-    this.get<EventResponse>(`/v1.2/events/${id}.json`);
+  fetchEvent = async (id: number) => {
+    const data = await this.get(`/v1.2/events/${id}.json`);
+    const response = EventResponse.decode(data);
+    if (isLeft(response)) throw toError(response.left);
+    return response.right;
+  };
 
-  fetchGenres = () => this.get<GenresResponse>('/v1.2/genres.json');
+  fetchGenres = async () => {
+    const data = await this.get('/v1.2/genres.json');
+    const response = GenresResponse.decode(data);
+    if (isLeft(response)) throw toError(response.left);
+    return response.right;
+  };
 
-  fetchLivers = () => this.get<LiversResponse>('/v1.2/livers.json');
+  fetchLivers = async () => {
+    const data = await this.get('/v1.2/livers.json');
+    const response = LiversResponse.decode(data);
+    if (isLeft(response)) throw toError(response.left);
+    return response.right;
+  };
 
-  fetchLiver = (id: number) =>
-    this.get<LiverResponse>(`/v1.2/livers/${id}.json`);
+  fetchLiver = async (id: number) => {
+    const data = await this.get(`/v1.2/livers/${id}.json`);
+    const response = LiverResponse.decode(data);
+    if (isLeft(response)) throw toError(response.left);
+    return response.right;
+  };
 }
