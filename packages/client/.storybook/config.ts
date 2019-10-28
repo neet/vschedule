16fbbf64bf6e6aeab79dc13c9c59891eeb560e5c
Neetshin <n33t5hin@gmail.com>
2019-10-28 13:18:33 +0900

    Improve loading indicator

--- packages/client/.storybook/config.ts
+++ packages/client/.storybook/config.ts
@@ -23,7 +23,7 @@ addParameters({
   },
 });
 
-const req = require.context('../src', true, /.stories.tsx$/);
+const req = require.context('../src', true, /\.stories\.tsx$/);
 const loadStories = () => {
   req.keys().forEach(req);
 };
