969a2eee1d09b4ec5339916e7190f3f0090c3c99
Ryo Igarashi <n33t5hin@gmail.com>
2021-01-02 19:05:04 +0900

    fix: Fix purge directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  purge: ['./src/**/*.tsx'],
   darkMode: 'media',
   theme: {
     colors: {
