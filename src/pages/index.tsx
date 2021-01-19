40fae4d9510dfc3a519b37d41cc99542c54400dc
Ryo Igarashi <n33t5hin@gmail.com>
2021-01-20 02:41:11 +0900

    feat: Add tag page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import dayjs from 'dayjs';
 import dynamic from 'next/dynamic';
 import { useMemo, useState } from 'react';
-import { useLocalStorage } from 'react-use';
+import { useLocalStorage, useSearchParam } from 'react-use';
 
 import { ChangeLog } from '../components/app/ChangeLog';
 import { Crown } from '../components/app/Crown';
@@ -22,10 +22,17 @@ const Timetable = dynamic<TimetableProps>(
 
 const GENRE_ALL = -1;
 
+// if you use this hook other than here, make this a module.
+const useGenreQueryParam = (): number | undefined => {
+  const queryGenre = useSearchParam('genre');
+  return queryGenre != null ? Number(queryGenre) : undefined;
+};
+
 const Events = (): JSX.Element => {
   const { data } = useEvents();
   const [swapDelta] = useLocalStorage<boolean>('swap-delta');
-  const [genre, setGenre] = useState(GENRE_ALL);
+  const genreQuery = useGenreQueryParam();
+  const [genre, setGenre] = useState(genreQuery ?? GENRE_ALL);
 
   const startAt = data ? dayjs(data.data.events[0].start_date) : dayjs();
   const endAt = data
--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,9 +1,17 @@
+import { faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 import { Layout } from '../components/app/Layout';
+import { useGenres } from '../components/hooks/useGenres';
+import { Card } from '../components/ui/Card';
 import { Typography } from '../components/ui/Typography';
 
 const Tags: NextPage = () => {
+  const { data } = useGenres();
+
   return (
     <Layout
       variant="article"
@@ -11,7 +19,38 @@ const Tags: NextPage = () => {
       description="このページは現在実装中です"
     >
       <Typography.H1>タグ別の配信一覧</Typography.H1>
-      <Typography.Paragraph>このページは現在実装中です。</Typography.Paragraph>
+      <Typography.Paragraph>
+        にじさんじのタグ別の配信一覧です。
+      </Typography.Paragraph>
+
+      {data?.data.genres.map((genre) => (
+        <Card key={genre.id}>
+          <Link href={`/?genre=${genre.id}`}>
+            <a className="block group">
+              <div className={classNames('flex', 'items-center', 'space-x-4')}>
+                <div
+                  className={classNames(
+                    'bg-primary-500',
+                    'text-white',
+                    'w-10',
+                    'h-10',
+                    'rounded-full',
+                    'flex',
+                    'justify-center',
+                    'items-center',
+                  )}
+                >
+                  <FontAwesomeIcon icon={faHashtag} />
+                </div>
+
+                <h3 className={classNames('group-hover:underline')}>
+                  {genre.name}
+                </h3>
+              </div>
+            </a>
+          </Link>
+        </Card>
+      ))}
     </Layout>
   );
 };
