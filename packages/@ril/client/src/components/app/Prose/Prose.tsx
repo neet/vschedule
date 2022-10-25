09a7c77e3fdff21b6e27225871017b2f73acf8cd
Ryo Igarashi <n33t5hin@gmail.com>
2022-10-25 22:55:07 +0900

    fix: Use react-headings level in MDXProvider

--- packages/@ril/client/src/components/app/Prose/Prose.tsx
+++ packages/@ril/client/src/components/app/Prose/Prose.tsx
@@ -1,5 +1,6 @@
 import { MDXProvider } from '@mdx-js/react';
 import type { ReactNode } from 'react';
+import { useLevel } from 'react-headings';
 
 import { Link } from '../../ui/Link';
 import { Typography } from '../../ui/Typography';
@@ -10,14 +11,17 @@ export interface ProseProps {
 
 export const Prose = (props: ProseProps): JSX.Element => {
   const { children } = props;
+  const baseLevel = useLevel().level;
 
   return (
     <MDXProvider
       components={{
-        h1: (props) => <Typography.FourXl as="h1" {...props} />,
-        h2: (props) => <Typography.ThreeXl as="h2" {...props} />,
-        h3: (props) => <Typography.TwoXl as="h3" {...props} />,
-        h4: (props) => <Typography.Xl as="h4" {...props} />,
+        h1: (props) => <Typography.FourXl as={`h${baseLevel}`} {...props} />,
+        h2: (props) => (
+          <Typography.ThreeXl as={`h${baseLevel + 1}`} {...props} />
+        ),
+        h3: (props) => <Typography.TwoXl as={`h${baseLevel + 2}`} {...props} />,
+        h4: (props) => <Typography.Xl as={`h${baseLevel + 3}`} {...props} />,
         p: Typography.Base,
         a: Link,
       }}
