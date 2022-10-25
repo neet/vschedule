e97830006740233c2fc57b3520746c6b7f839e3a
Ryo Igarashi <n33t5hin@gmail.com>
2022-10-25 22:56:58 +0900

    chore: Prettify

--- packages/@ril/client/src/components/app/Prose/Prose.tsx
+++ packages/@ril/client/src/components/app/Prose/Prose.tsx
@@ -1,5 +1,5 @@
 import { MDXProvider } from '@mdx-js/react';
-import type { ReactNode } from 'react';
+import { ReactNode, useCallback } from 'react';
 import { useLevel } from 'react-headings';
 
 import { Link } from '../../ui/Link';
@@ -11,17 +11,20 @@ export interface ProseProps {
 
 export const Prose = (props: ProseProps): JSX.Element => {
   const { children } = props;
-  const baseLevel = useLevel().level;
+  const { level } = useLevel();
+
+  const getHeading = useCallback(
+    (relativeLevel: number) => 'h' + Math.max(level + relativeLevel, 6),
+    [level],
+  );
 
   return (
     <MDXProvider
       components={{
-        h1: (props) => <Typography.FourXl as={`h${baseLevel}`} {...props} />,
-        h2: (props) => (
-          <Typography.ThreeXl as={`h${baseLevel + 1}`} {...props} />
-        ),
-        h3: (props) => <Typography.TwoXl as={`h${baseLevel + 2}`} {...props} />,
-        h4: (props) => <Typography.Xl as={`h${baseLevel + 3}`} {...props} />,
+        h1: (props) => <Typography.FourXl as={getHeading(0)} {...props} />,
+        h2: (props) => <Typography.ThreeXl as={getHeading(1)} {...props} />,
+        h3: (props) => <Typography.TwoXl as={getHeading(2)} {...props} />,
+        h4: (props) => <Typography.Xl as={getHeading(3)} {...props} />,
         p: Typography.Base,
         a: Link,
       }}
