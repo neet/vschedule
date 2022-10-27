a0babd74b2039ed82762da3d9f5dd3a705016659
Ryo Igarashi <n33t5hin@gmail.com>
2022-10-27 17:49:35 +0900

    fix: Fix base directory of storybook

--- packages/@ril/client/.storybook/main.js
+++ packages/@ril/client/.storybook/main.js
@@ -2,7 +2,10 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/components/**/*.stories.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
