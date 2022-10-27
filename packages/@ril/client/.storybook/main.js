18355f0385cbf66a97e2451064960d1320e95958
Ryo Igarashi <n33t5hin@gmail.com>
2022-10-27 17:30:18 +0900

    fix: Fix storybook config

--- packages/@ril/client/.storybook/main.js
+++ packages/@ril/client/.storybook/main.js
@@ -1,27 +1,21 @@
-module.exports = {
-  stories: ['../src/components/**/*.stories.tsx'],
+mrodule.exports = {
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs',
-    // apply PostCSS 8 until the major release of Sb 6.2.0
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+
     {
-      name: '@storybook/addon-postcss',
+      name: "@storybook/addon-postcss",
       options: {
         postcssLoaderOptions: {
-          implementation: require('postcss'),
+          implementation: require("postcss"),
         },
       },
     },
   ],
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
+  framework: "@storybook/react",
 };
