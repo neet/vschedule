41fbc687aec8e17c5641dec61c54555405483725
Ryo Igarashi <n33t5hin@gmail.com>
2022-10-27 17:40:43 +0900

    fix: Fix syntax error

--- packages/@ril/client/.storybook/main.js
+++ packages/@ril/client/.storybook/main.js
@@ -1,21 +1,21 @@
-mrodule.exports = {
+module.exports = {
   core: {
-    builder: "webpack5",
+    builder: 'webpack5',
   },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
 
     {
-      name: "@storybook/addon-postcss",
+      name: '@storybook/addon-postcss',
       options: {
         postcssLoaderOptions: {
-          implementation: require("postcss"),
+          implementation: require('postcss'),
         },
       },
     },
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
 };
