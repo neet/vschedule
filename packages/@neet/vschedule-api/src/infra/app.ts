c22ce2e38b7f6008ed692a25275c6e2f8ecfb183
Ryo Igarashi <n33t5hin@gmail.com>
2022-11-11 21:18:30 +0900

    chore: Remove unused OIDC

--- packages/@neet/vschedule-api/src/infra/app.ts
+++ packages/@neet/vschedule-api/src/infra/app.ts
@@ -20,7 +20,6 @@ import { TYPES } from '../types';
 import { appErrorHandler } from './middlewares/AppErrorHandler';
 import { domainErrorHandler } from './middlewares/DomainErrorHandler';
 import { openapiErrorHandler } from './middlewares/OpenApiErrorHandler';
-import { provider } from './oidc';
 
 /**
  * Create app by given container
@@ -36,9 +35,6 @@ export const createApp = (container: Container): Application => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(expressWinston.logger({ winstonInstance: logger }));
-
-    // OpenID Connect
-    app.use('/oidc', provider.callback());
 
     // OpenAPI Documentation
     app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
